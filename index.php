5bbc87b3bc94dec189231baaa58c11bed54ca245
unknown <imrantt@gmail.com>
2022-08-19 18:54:44 +0500

    First Time added

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>  NOON E-COMMERCE</title>
+    <link href="fa/svgs/regular/address-book.svg">
+    <link href="css/jquery.bxslider.min.css" rel="stylesheet">
+    <link href="css/owl.carousel.min.css" rel="stylesheet">
+    <link href="css/owl.theme.default.min.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    
+    </head>
+    <body>
+        <div class="wrapper">
+        
+        <div class="navigation-bar" >
+            
+            
+          
+                
+            
+            
+            
+            
+            
+             
+             
+                 
+            
+            
+        
+            
+           
+            <div class="container">
+                 
+                
+                
+                
+                <div class="row">
+                    
+                    <div class="col-4">
+                     <div class="logo">
+                    <img src="images/noon-black-en.svg%20logo.svg" alt="noon" class="noonlogo" height="75px" width="50%">
+                   
+                    
+                </div>
+               
+                    
+                    </div>
+                                        <div class="col-4">
+                                             
+                       
+                       <form action="" method="post">
+                           
+                       <div class="Search_box">
+                      
+                           
+                       <input type="text" name="Search_box" class="search-box";>
+                           
+                        
+                  
+                           
+                       
+                           
+                          
+                      
+                   
+                   
+                   
+                   
+                   </div>
+                                               
+                                                  
+                                                  
+                        
+                    
+                
+                    
+                    
+                    
+                    </form>
+                    
+                  
+                    
+                    </div>
+                    <div class="user-menu">
+                        <ul>
+                        <li><a href="#" class="dorpdown-toggle " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >My Account</a></li>
+                        <li><a href="#" > Cart</a></li>    
+                        </ul>
+                    
+                    </div>
+
+                    
+                   
+                </div>
+                
+                
+               
+               
+                
+             
+                
+               
+            </div>
+            
+            
+            
+            
+            
+            </div>
+        
+        
+        </div>
+        
+        
+        
+        
+        <script src="js/jquery.js"></script>
+        <script src="js/jquery.bxslider.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/main.js"></script>
+        <script src="fa/svgs/regular/address-book.svg"></script>
+    
+    </body>
+
+
+
+</html>
